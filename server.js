97817aa08354e26a4f9e0b00f2fdc9d31d59b592
ggandara8 <ggandara8@gmail.com>
2020-07-29 13:56:36 -0500

    folders and files created

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const logger = require("morgan");
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.use(logger("dev"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+  useNewUrlParser: true,
+  useFindAndModify: false
+});
+
+// api and html routes
+app.use(require("./routes/api-routes"));
+app.use(require("./routes/html-routes"));
+
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}!`);
+});
